844dc4e937029fe60be9441b20f3f8e3d4522ad0
M Ganeshkumar <35286288+mgkumar138@users.noreply.github.com>
2020-12-29 18:00:15 +0800

    Update actor_critic_tf.py

--- actor_critic_tf.py
+++ actor_critic_tf.py
@@ -1,38 +1,21 @@
-from keras.layers import Dense, Input
-from keras.models import Model
-from keras.optimizers import Adam
 import numpy as np
 import tensorflow as tf
 import gym
-from keras import backend as K
-from keras.utils.np_utils import to_categorical
 import matplotlib.pyplot as plt
 import time as dt
-from tensorflow.python import keras
-from tensorflow.python.keras import layers
-import threading
-
-tf.enable_eager_execution()
-
-'''
-Batch actor critic algorithm 
-Advantage = current reward + y*future reward - value of current state
-train on batch 
-
-Implementation of online actor-critic?
-'''
+import os
 
 
-class ActorCriticModel(keras.Model):
+class ActorCriticModel(tf.keras.Model):
     def __init__(self, state_size, action_size):
         super(ActorCriticModel, self).__init__()
         self.state_size = state_size
         self.action_size = action_size
 
-        self.dense1 = layers.Dense(24, activation='relu')
-        self.dense2 = layers.Dense(48, activation='relu')
-        self.logits = layers.Dense(action_size)
-        self.values = layers.Dense(1)
+        self.dense1 = tf.keras.layers.Dense(128, activation='relu')
+        self.dense2 = tf.keras.layers.Dense(128, activation='relu')
+        self.logits = tf.keras.layers.Dense(action_size)
+        self.values = tf.keras.layers.Dense(1)
 
     def call(self, inputs):
         A1 = self.dense1(inputs)
@@ -46,16 +29,15 @@ class ActorCriticModel(keras.Model):
 
 
 class ACagent:
-    save_lock = threading.Lock()
 
     def __init__(self, env):
-        self.gamma = 0.99
+        self.gamma = 0.95
         self.lr = 0.001
         self.state_size = env.observation_space.shape[0]
         self.action_size = env.action_space.n
 
         self.model = ActorCriticModel(self.state_size, self.action_size)
-        self.opt = tf.train.AdamOptimizer(self.lr)
+        self.opt = tf.optimizers.Adam(self.lr)
 
     def replay(self, memory):
         discount_reward = self.discount_normalise_rewards(memory.rewards)
@@ -69,7 +51,7 @@ class ACagent:
         return policy_loss, value_loss, total_loss
 
     def act(self, state):
-        action_dist, _ = self.model(tf.convert_to_tensor(state[None, :], dtype=tf.float32))
+        action_dist, _ = self.model(tf.cast(state[None, :], dtype=tf.float32))
         action_prob_dist = tf.nn.softmax(action_dist)
         return action_prob_dist.numpy(), np.random.choice(range(self.action_size), p=action_prob_dist.numpy()[0])
 
@@ -88,24 +70,18 @@ class ACagent:
         logit, values = self.model(tf.convert_to_tensor(np.vstack(memory.states), dtype=tf.float32))
 
         # distinguish AC to A2C. AC -> use only values/discounted rewards. Advantage = Q(s,a) - V(s)
-        advantage = tf.convert_to_tensor(np.array(discounted_rewards)[None,:], dtype=tf.float32) - values
+        advantage = tf.cast(np.array(discounted_rewards), dtype=tf.float32) - values[:,0]
 
         value_loss = advantage**2
 
         # compute actor policy loss with critic input as advantage
-        neg_log_prob = tf.nn.softmax_cross_entropy_with_logits_v2(logits=logit, labels=np.array(memory.actions)[None,:]) # Nx1 tensor
+        neg_log_prob = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logit, labels=np.array(memory.actions))
 
         policy_loss = neg_log_prob * tf.stop_gradient(advantage) # becomes NxN tensor
 
-        entropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits=logit, labels=tf.nn.softmax(logit))
+        entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logit, labels=tf.nn.softmax(logit))
 
         policy_loss_minent = policy_loss - 0.01 * entropy
-
-        # custom loss
-        cross_entropy = memory.actions * tf.log(tf.nn.softmax(logit))
-        neg_log_prob2 = -tf.reduce_sum(cross_entropy, 1)
-
-        policy_loss2 = neg_log_prob * advantage # NxN tensor
 
         # merge both losses to train network tgt
         comb_loss = tf.reduce_mean((0.5 * value_loss + policy_loss_minent))
@@ -138,7 +114,7 @@ def main(episodes):
     print('++ Training started on {} at {} ++'.format(dt.strftime('%d%m%Y'), dt.strftime('%H.%M.%S')))
     start_time = dt.time()
 
-    time = 250
+    time = 200
     tot_r = []
     tot_loss = []
 
@@ -153,9 +129,7 @@ def main(episodes):
 
             new_state, reward, done, _ = env.step(action)
 
-            onehot_action = to_categorical(action, agent.action_size)
-
-            mem.store(state, onehot_action, reward)
+            mem.store(state, action, reward)
 
             state = new_state
 
@@ -168,9 +142,9 @@ def main(episodes):
 
         meanR = np.mean(tot_r)
         maxR = np.amax(tot_r)
-        print('Episode {} of {}, last for {}s, Mean Reward {}, Max Reward {}, Training Loss {}'.format(e, episodes,
-                                                                                                       tot_r[-1], meanR,
-                                                                                                       maxR, tot_loss[-1]))
+        print('E {}/{}, last for {}s, Mean R {:.4f}, Max R {:.4f}, Tr Loss {:.4f}'.format(
+            e, episodes, tot_r[-1], meanR,maxR, tot_loss[-1]))
+
     date = dt.strftime('%d%m%Y')
     clock = dt.strftime('%H.%M.%S')
     print('Training ended on {} at {}'.format(date, clock))
@@ -187,38 +161,31 @@ def main(episodes):
     plt.title('A2C CartPole {}eps {} {}'.format(episodes, date, clock))
     plt.show()
     '''
-
-    ACagent.model.save_weights
     return tot_r, tot_loss, run_time
 
 
 if __name__ == "__main__":
     strap_reward, strap_loss, strap_run = [],[],[]
-    plt.figure(1)
-    episodes = 5000
-    bootstrap = 100
+
+    episodes = 2000
+    bootstrap = 1
     for i in range(bootstrap):
         print('Bootstrap {} of {}'.format(i+1, bootstrap))
         total_reward, total_loss, runtime = main(episodes=episodes)
         strap_reward.append(total_reward)
         strap_loss.append(total_loss)
         strap_run.append(runtime)
-        plt.plot(list(range(1, episodes + 1)), strap_reward[i])
-
-
-    plt.figure(2)
+        #plt.plot(list(range(1, episodes + 1)), strap_reward[i])
+    plt.figure()
+    plt.subplot(211)
     plt.plot(list(range(1, episodes + 1)), np.mean(np.array(strap_reward),0))
     plt.plot(list(range(1, episodes + 1)),
              np.convolve(np.mean(np.array(strap_reward),0), np.ones(100) / 100, mode='same'))
     plt.title('A2C CartPole reward, Total Runtime {}s'.format(np.round(np.sum(strap_run),3)))
 
-    plt.figure(3)
+    plt.subplot(212)
     plt.plot(list(range(1, episodes + 1)), np.mean(np.array(strap_loss),0))
     plt.plot(list(range(1, episodes + 1)),
              np.convolve(np.mean(np.array(strap_loss), 0), np.ones(100) / 100, mode='same'))
     plt.title('A2C CartPole loss, Total Runtime {}s'.format(np.round(np.sum(strap_run),3)))
-
-    plt.figure(4)
-    plt.plot(list(range(1, bootstrap + 1)), strap_run)
-    plt.title('A2C CartPole, Average Runtime {}s'.format(np.round(np.mean(strap_run),3)))
-
+    plt.show()
